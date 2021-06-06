805215eba8ad13181bb449445289d288634da35d
manhvu0803 <31726319+manhvu0803@users.noreply.github.com>
2021-06-06 09:33:55 +0700

    Create Event and Item tile classes
    Declare basic functions for EventTile and ItemTile class

---
+++ Assets/Scripts/Tile/ItemTile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTile : Tile
+{
+	public override void OnDiscover()
+	{
+		Debug.Log("Draw an Omen");
+	}
+}
