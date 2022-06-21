072f9993d6645cc59a4fdcd1e15f2d8f4f049138
Nguyễn Mạnh Vũ <31726319+manhvu0803@users.noreply.github.com>
2022-06-21 09:06:24 +0700

    Fix TilePool and reimport packages
    Squashing commit and use the wrong Unity version is dangerous

--- Assets/Scripts/Tile/TilePool.cs
+++ Assets/Scripts/Tile/TilePool.cs
@@ -6,93 +6,15 @@ using Mirror;
 using UnityEditor.SceneManagement;
 #endif
 
-public class TilePool : MonoBehaviour
+public class TilePool : Pool<Tile>
 {
-	private class TileItem
-	{
-		public Tile tile;
-
-		public float weight;
-
-		public bool used;
-
-		public TileItem(Tile obj, float w = 0)
-		{
-			tile = obj;
-			weight = w;
-			used = false;
-		}
-
-		public override string ToString() => $"{tile} used:{used}";
-	}
-
-	private TileItem[] _tilePool;
-
-	[SerializeField]
-	private Tile[] _tileList;
-
 	#if UNITY_EDITOR
-	[ContextMenu("Get tile prefabs from Resources")]
+	[ContextMenu("Get tile prefabs from Assets/Prefabs/Tiles")]
 	private void GetTileDataResources()
 	{
 		string[] directories = { "Assets/Prefabs/Tiles" };
-		_tileList = Utilities.GetAsset<Tile>("t:prefab", directories).ToArray();
+		_itemList = Utilities.GetAsset<Tile>("t:prefab", directories).ToArray();
 		EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
 	}
 	#endif
-
-	private void Start()
-	{
-		_tilePool = new TileItem[_tileList.Length];
-		
-		for (int i = 0; i < _tileList.Length; ++i) 
-		{
-			//NetworkClient.RegisterPrefab(tile.gameObject);
-			_tilePool[i] = new TileItem(_tileList[i], UnityEngine.Random.Range(0, 50));
-		}
-		Array.Sort(_tilePool, (a, b) => a.weight.CompareTo(b.weight));
-	}
-
-	public void ResetPool()
-	{
-		foreach (TileItem item in _tilePool) 
-		{
-			item.tile.gameObject.SetActive(false);
-			item.used = false;
-			item.weight = UnityEngine.Random.Range(0, 50);
-		}
-
-		Array.Sort(_tilePool, (a, b) => a.weight.CompareTo(b.weight));
-	}
-
-	public Tile GetRandomTile()
-	{
-		int i = 0;
-		while (i < _tilePool.Length && _tilePool[i].used)
-			++i;
-
-		if (_tilePool.Length <= i) {
-			Debug.Log("Out of tiles");
-			return null;
-		}
-		_tilePool[i].used = true;
-		_tilePool[i].tile.gameObject.SetActive(true);
-		return _tilePool[i].tile;
-	}
-
-	public Tile GetRandomTile(Func<TileLocation, bool> chooser)
-	{
-		int i = 0;
-		while (i < _tilePool.Length && (_tilePool[i].used || !chooser(_tilePool[i].tile.Location)))
-			++i;
-
-		if (i >= _tilePool.Length) {
-			Debug.Log($"Out of tiles for {chooser.Target}");
-			return null;
-		}
-
-		_tilePool[i].used = true;
-		_tilePool[i].tile.gameObject.SetActive(true);
-		return _tilePool[i].tile;
-	}
 }
